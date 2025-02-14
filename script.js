3d43673984c425c26304c0c814d0889c46e62c06
Vinay Tiwari <vinaytiwari77485@gmail.com>
2025-02-14 10:15:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function validation(){
+   
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;
+
+
+   if( username =="Vinay" || password =="1234")
+   {
+      alert("Login successfull");
+
+   }
+   else{      
+      alert("Invalid username and password")
+   }
+}
