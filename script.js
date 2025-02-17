beca463b26156c20a5d9378c2a1c8083753ddd6d
Vinay Tiwari <vinaytiwari77485@gmail.com>
2025-02-17 09:35:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let username = document.getElementById('username').value;
 let password = document.getElementById('password').value;
 
 
-   if( username =="Vinay" || password =="1234")
+   if( username =="Vinay" && password =="1234")
    {
       alert("Login successfull");
 
